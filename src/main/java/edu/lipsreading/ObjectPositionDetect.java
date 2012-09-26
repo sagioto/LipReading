0f9ae4c871fd94ff4aefd65549e5a3ab0547bd66
Dor Leitman <dorleitman@gmail.com>
2012-09-26 20:13:17 +0200

    ObjectPositionDetect class was added
    This class is able to detect red pixels position.
    A test with some pictures of mine with red sticker on my lips were
    passed successfully.

---
+++ src/main/java/edu/lipsreading/ObjectPositionDetect.java
@@ -0,0 +1,66 @@
+package edu.lipsreading;
+
+//imports
+import static com.googlecode.javacv.cpp.opencv_core.cvCreateImage;
+import static com.googlecode.javacv.cpp.opencv_core.cvGetSize;
+import static com.googlecode.javacv.cpp.opencv_core.cvInRangeS;
+import static com.googlecode.javacv.cpp.opencv_core.cvReleaseImage;
+import static com.googlecode.javacv.cpp.opencv_core.cvScalar;
+import static com.googlecode.javacv.cpp.opencv_highgui.cvLoadImage;
+import static com.googlecode.javacv.cpp.opencv_highgui.cvSaveImage;
+import static com.googlecode.javacv.cpp.opencv_imgproc.CV_BGR2HSV;
+import static com.googlecode.javacv.cpp.opencv_imgproc.CV_MEDIAN;
+import static com.googlecode.javacv.cpp.opencv_imgproc.cvCvtColor;
+import static com.googlecode.javacv.cpp.opencv_imgproc.cvSmooth;
+import static com.googlecode.javacv.cpp.opencv_imgproc.cvGetSpatialMoment;
+import static com.googlecode.javacv.cpp.opencv_imgproc.cvGetCentralMoment;
+import static com.googlecode.javacv.cpp.opencv_imgproc.cvMoments;
+import java.awt.Dimension;
+
+import com.googlecode.javacv.cpp.opencv_core.IplImage;
+import com.googlecode.javacv.cpp.opencv_imgproc.CvMoments;
+
+public class ObjectPositionDetect {
+  static int hueLowerR = 160;
+  static int hueUpperR = 180;
+
+  public static void main(String[] args) {
+      IplImage orgImg = cvLoadImage("aba7.bmp");
+      IplImage thresholdImage = hsvThreshold(orgImg);
+      cvSaveImage("hsvthreshold.jpg", thresholdImage);
+      Dimension position = getCoordinates(thresholdImage);
+      System.out.println("Dimension of original Image : " + thresholdImage.width() + " , " + thresholdImage.height());
+      System.out.println("Position of red spot    : x : " + position.width + " , y : " + position.height);
+  }
+
+  static Dimension getCoordinates(IplImage thresholdImage) {
+      int posX = 0;
+      int posY = 0;
+      CvMoments moments = new CvMoments();
+      cvMoments(thresholdImage, moments, 1);
+      // cv Spatial moment : Mji=sumx,y(I(x,y)•xj•yi)
+      // where I(x,y) is the intensity of the pixel (x, y).
+      double momX10 = cvGetSpatialMoment(moments, 1, 0); // (x,y)
+      double momY01 = cvGetSpatialMoment(moments, 0, 1);// (x,y)
+      double area = cvGetCentralMoment(moments, 0, 0);
+      posX = (int) (momX10 / area);
+      posY = (int) (momY01 / area);
+      return new Dimension(posX, posY);
+  }
+
+  static IplImage hsvThreshold(IplImage orgImg) {
+      // 8-bit, 3- color =(RGB)
+      IplImage imgHSV = cvCreateImage(cvGetSize(orgImg), 8, 3);
+      System.out.println(cvGetSize(orgImg));
+      cvCvtColor(orgImg, imgHSV, CV_BGR2HSV);
+      // 8-bit 1- color = monochrome
+      IplImage imgThreshold = cvCreateImage(cvGetSize(orgImg), 8, 1);
+      // cvScalar : ( H , S , V, A)
+      cvInRangeS(imgHSV, cvScalar(hueLowerR, 100, 100, 0), cvScalar(hueUpperR, 255, 255, 0), imgThreshold);
+      cvReleaseImage(imgHSV);
+      cvSmooth(imgThreshold, imgThreshold, CV_MEDIAN, 13);
+      // save
+      return imgThreshold;
+  }
+}
+
