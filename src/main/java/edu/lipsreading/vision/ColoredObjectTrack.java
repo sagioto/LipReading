6546382f5805d0e0c10ea87f2df150135ccfdbac
Dor Leitman <dorleitman@gmail.com>
2012-10-31 10:00:08 +0200

    Added ColoredObjectTrack class
    Added ColoredObjectTrack class - tracking colored objects, such as
    stickers.

---
+++ src/main/java/edu/lipsreading/vision/ColoredObjectTrack.java
@@ -0,0 +1,217 @@
+package edu.lipsreading.vision;
+
+import static com.googlecode.javacv.cpp.opencv_core.IPL_DEPTH_8U;
+import static com.googlecode.javacv.cpp.opencv_core.cvCreateImage;
+import static com.googlecode.javacv.cpp.opencv_core.cvFlip;
+import static com.googlecode.javacv.cpp.opencv_core.cvGetSize;
+import static com.googlecode.javacv.cpp.opencv_core.cvInRangeS;
+import static com.googlecode.javacv.cpp.opencv_core.cvScalar;
+import static com.googlecode.javacv.cpp.opencv_imgproc.CV_BGR2GRAY;
+import static com.googlecode.javacv.cpp.opencv_imgproc.CV_MEDIAN;
+import static com.googlecode.javacv.cpp.opencv_imgproc.cvCvtColor;
+import static com.googlecode.javacv.cpp.opencv_imgproc.cvEqualizeHist;
+import static com.googlecode.javacv.cpp.opencv_imgproc.cvGetCentralMoment;
+import static com.googlecode.javacv.cpp.opencv_imgproc.cvGetSpatialMoment;
+import static com.googlecode.javacv.cpp.opencv_imgproc.cvMoments;
+import static com.googlecode.javacv.cpp.opencv_imgproc.cvSmooth;
+import static com.googlecode.javacv.cpp.opencv_highgui.*;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JPanel;
+
+import com.googlecode.javacv.CanvasFrame;
+import com.googlecode.javacv.FrameGrabber;
+import com.googlecode.javacv.VideoInputFrameGrabber;
+import com.googlecode.javacv.cpp.opencv_core.CvScalar;
+import com.googlecode.javacv.cpp.opencv_core.IplImage;
+import com.googlecode.javacv.cpp.opencv_imgproc.CvMoments;
+
+public class ColoredObjectTrack implements Runnable {
+    final int INTERVAL = 1000;// 1sec
+    final int CAMERA_NUM = 0; // Default camera for this time
+
+    /**
+     * Correct the color range- it depends upon the object, camera quality,
+     * environment.
+     */
+    static CvScalar red_min = cvScalar(0, 0, 130, 0);// RED 
+    static CvScalar red_max = cvScalar(80, 80, 255, 0);
+    
+    static CvScalar green_min = cvScalar(0, 40, 0, 0);
+    static CvScalar green_max = cvScalar(50, 255, 50, 0);
+
+    static CvScalar blue_min = cvScalar(150, 0, 0, 0);
+    static CvScalar blue_max = cvScalar(255, 150, 150, 0);
+    
+    static CvScalar yellow_min = cvScalar(0, 120, 120, 0);
+    static CvScalar yellow_max = cvScalar(90, 255, 255, 0);
+    
+    
+    static Color red = Color.RED;
+    static Color green = Color.GREEN;
+    static Color blue = Color.BLUE;
+    static Color yellow = Color.YELLOW;
+    
+    
+    IplImage image;
+    CanvasFrame canvas = new CanvasFrame("Web Cam Live");
+    CanvasFrame path = new CanvasFrame("Detection");
+    int ii = 0;
+    JPanel jp = new JPanel();
+
+    public ColoredObjectTrack() {
+        canvas.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+        path.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+        path.setContentPane(jp);
+        
+    }
+
+    @Override
+    public void run() {
+        FrameGrabber grabber = new VideoInputFrameGrabber(CAMERA_NUM);
+        try {
+            grabber.start();
+            IplImage img;
+            int posX = 0;
+            int posY = 0;
+            while (true) {
+                img = grabber.grab();
+                path.setSize(img.width(), img.height());
+                if (img != null) {
+                    // show image on window
+                    cvFlip(img, img, 1);// l-r = 90_degrees_steps_anti_clockwise
+                    canvas.showImage(img);
+                    //IplImage detectThrs = getThresholdImage(img);
+                    /*
+                    // Option 1 - show first red, then blue, then green & yellow
+                    showObject(img, red_min, red_max, red);
+                    showObject(img, blue_min, blue_max, blue);
+                    showObject(img, green_min, green_max, green);
+                    showObject(img, yellow_min, yellow_max, yellow);
+                    */
+                    
+                    // Option 2 - get threshold img of all colors & paint them together:
+                    int[] redCoordinates = getCoordinatesOfObject(img, red_min, red_max);
+                    int[] blueCoordinates = getCoordinatesOfObject(img, blue_min, blue_max);
+                    int[] greenCoordinates = getCoordinatesOfObject(img, green_min, green_max);
+                    int[] yellowCoordinates = getCoordinatesOfObject(img, yellow_min, yellow_max);
+                    
+                    Graphics redDot = jp.getGraphics();
+                    redDot.clearRect(0, 0, img.width(), img.height());
+                    redDot.setColor(red);
+                    redDot.fillOval(redCoordinates[0],redCoordinates[1], 10, 10);
+                    
+                    Graphics blueDot = jp.getGraphics();
+                    blueDot.clearRect(0, 0, img.width(), img.height());
+                    blueDot.setColor(blue);
+                    blueDot.fillOval(blueCoordinates[0], blueCoordinates[1], 10, 10);
+                    
+                    Graphics greenDot = jp.getGraphics();
+                    greenDot.clearRect(0, 0, img.width(), img.height());
+                    greenDot.setColor(green);
+                    greenDot.fillOval(greenCoordinates[0], greenCoordinates[1], 10, 10);
+                    
+                    Graphics yellowDot = jp.getGraphics();
+                    yellowDot.clearRect(0, 0, img.width(), img.height());
+                    yellowDot.setColor(yellow);
+                    //yellowDot.fillOval(yellowCoordinates[0], yellowCoordinates[1], 10, 10);
+                    
+                    redDot.drawOval(redCoordinates[0],redCoordinates[1], 10, 10);
+                    blueDot.drawOval(blueCoordinates[0],blueCoordinates[1], 10, 10);
+                    yellowDot.drawOval(yellowCoordinates[0],yellowCoordinates[1], 10, 10);
+                    greenDot.drawOval(greenCoordinates[0],greenCoordinates[1], 10, 10);
+                    
+                }
+                // Thread.sleep(INTERVAL);
+            }
+        } catch (Exception e) {
+        }
+    }
+
+	private int[] getCoordinatesOfObject(IplImage img, CvScalar scalarMin,
+			CvScalar scalarMax) {
+		int posX;
+		int posY;
+		IplImage detectThrs = getThresholdImage(img, scalarMin, scalarMax);
+		CvMoments moments = new CvMoments();
+		cvMoments(detectThrs, moments, 1);
+		double mom10 = cvGetSpatialMoment(moments, 1, 0);
+		double mom01 = cvGetSpatialMoment(moments, 0, 1);
+		double area = cvGetCentralMoment(moments, 0, 0);
+		posX = (int) (mom10 / area);
+		posY = (int) (mom01 / area);
+		int[] res = {posX, posY}; 
+		return res;
+	}
+
+	private void showObject(IplImage img, CvScalar scalarMin, CvScalar scalarMax, Color color) {
+		int posX;
+		int posY;
+		IplImage detectThrs = getThresholdImage(img, scalarMin, scalarMax);
+		CvMoments moments = new CvMoments();
+		cvMoments(detectThrs, moments, 1);
+		double mom10 = cvGetSpatialMoment(moments, 1, 0);
+		double mom01 = cvGetSpatialMoment(moments, 0, 1);
+		double area = cvGetCentralMoment(moments, 0, 0);
+		posX = (int) (mom10 / area);
+		posY = (int) (mom01 / area);
+		// only if its a valid position
+		if (posX > 0 && posY > 0) {
+		    paint(img, posX, posY, color);
+		}
+	}
+
+    private void paint(IplImage img, int posX, int posY, Color color) {
+        Graphics g = jp.getGraphics();
+        path.setSize(img.width(), img.height());
+
+        g.clearRect(0, 0, img.width(), img.height());
+        g.setColor(color);
+        g.fillOval(posX, posY, 10, 10);
+        g.drawOval(posX, posY, 10, 10);
+       // System.out.println(posX + " , " + posY);
+        
+
+    }
+
+    private IplImage getThresholdImage(IplImage orgImg) {
+
+    	
+    	IplImage imgThreshold = cvCreateImage(cvGetSize(orgImg), 8, 1);
+        //
+        cvInRangeS(orgImg, red_min, red_max, imgThreshold);// red
+
+        cvSmooth(imgThreshold, imgThreshold, CV_MEDIAN, 15);
+        cvSaveImage(++ii + "dsmthreshold.jpg", imgThreshold);
+        return imgThreshold;
+    }
+    
+    private IplImage getThresholdImage(IplImage orgImg, CvScalar minScalar, CvScalar maxScalar) {
+        IplImage imgThreshold = cvCreateImage(cvGetSize(orgImg), 8, 1);
+        //
+        cvInRangeS(orgImg, minScalar, maxScalar, imgThreshold);// Color chosen
+
+        cvSmooth(imgThreshold, imgThreshold, CV_MEDIAN, 15);
+        //cvSaveImage(++ii + "dsmthreshold.jpg", imgThreshold);
+    	//cvSaveImage((++ii)+"-aa.jpg", orgImg);
+        return imgThreshold;
+    }
+
+    public static void main(String[] args) {
+        ColoredObjectTrack cot = new ColoredObjectTrack();
+        Thread th = new Thread(cot);
+        th.start();
+    }
+
+    public IplImage Equalize(BufferedImage bufferedimg) {
+        IplImage iploriginal = IplImage.createFrom(bufferedimg);
+        IplImage srcimg = IplImage.create(iploriginal.width(), iploriginal.height(), IPL_DEPTH_8U, 1);
+        IplImage destimg = IplImage.create(iploriginal.width(), iploriginal.height(), IPL_DEPTH_8U, 1);
+        cvCvtColor(iploriginal, srcimg, CV_BGR2GRAY);
+        cvEqualizeHist(srcimg, destimg);
+        return destimg;
+    }
+}
--- src/main/java/edu/lipsreading/vision/ObjectPositionDetect.java
+++ src/main/java/edu/lipsreading/vision/ObjectPositionDetect.java
@@ -16,7 +16,6 @@ import static com.googlecode.javacv.cpp.opencv_imgproc.cvGetSpatialMoment;
 import static com.googlecode.javacv.cpp.opencv_imgproc.cvGetCentralMoment;
 import static com.googlecode.javacv.cpp.opencv_imgproc.cvMoments;
 import java.awt.Dimension;
-import java.io.File;
 
 import com.googlecode.javacv.cpp.opencv_core.IplImage;
 import com.googlecode.javacv.cpp.opencv_imgproc.CvMoments;
@@ -26,14 +25,56 @@ public class ObjectPositionDetect {
   static int hueUpperR = 180;
 
   public static void main(String[] args) {
-	  File file = new File("src/main/resources/images/aba7.bmp");
-      IplImage orgImg = cvLoadImage(file.getAbsolutePath());
-      IplImage thresholdImage = hsvThreshold(orgImg);
-      cvSaveImage("hsvthreshold.jpg", thresholdImage);
-      Dimension position = getCoordinates(thresholdImage);
-      System.out.println("Dimension of original Image : " + thresholdImage.width() + " , " + thresholdImage.height());
-      System.out.println("Position of red spot    : x : " + position.width + " , y : " + position.height);
+	  /*
+	  String picName = "Photo_000";
+	  
+	  for (int i=44; i < 93; i++)
+	  {
+	  
+		  analyzePicture(picName + i + ".bmp");
+	  }*/
+	  analyzePicture("colors.bmp");
+      
   }
+
+private static void analyzePicture(String picName) {
+	IplImage orgImg = cvLoadImage(picName);
+      
+      // Original:
+      //IplImage thresholdImage = hsvThreshold(orgImg);
+      //cvSaveImage("hsvthreshold.jpg", thresholdImage);
+      //Dimension position = getCoordinates(thresholdImage);
+      //System.out.println("Dimension of original Image : " + thresholdImage.width() + " , " + thresholdImage.height());
+      //System.out.println("Position of red spot    : x : " + position.width + " , y : " + position.height);
+      
+    	System.out.println("Picture: " + picName);
+      
+      IplImage thresholdImage;
+      // Green:
+      thresholdImage = hsvThresholdByPx(orgImg, 38, 100, 50, 78, 255, 255);
+      
+      printCoordinates(thresholdImage, "Red");
+      
+      // blue:
+      thresholdImage = hsvThresholdByPx(orgImg, 100, 100, 100, 159, 255, 255);
+      printCoordinates(thresholdImage, "Blue");
+      
+      
+      // Yellow:
+      thresholdImage = hsvThresholdByPx(orgImg, 30, 100, 100, 99, 255, 255);
+      printCoordinates(thresholdImage, "Yellow");
+      
+      // Pink:
+      thresholdImage = hsvThresholdByPx(orgImg, 160, 100, 100, 255, 255, 255);
+      printCoordinates(thresholdImage, "Pink");
+}
+
+private static void printCoordinates(IplImage thresholdImage, String color) {
+	cvSaveImage("hsvthreshold"+color+".jpg", thresholdImage);
+      Dimension position = getCoordinates(thresholdImage);
+      
+      System.out.println(color + "-  x : " + position.width + " , y : " + position.height);
+}
 
   static Dimension getCoordinates(IplImage thresholdImage) {
       int posX = 0;
@@ -53,12 +94,27 @@ public class ObjectPositionDetect {
   static IplImage hsvThreshold(IplImage orgImg) {
       // 8-bit, 3- color =(RGB)
       IplImage imgHSV = cvCreateImage(cvGetSize(orgImg), 8, 3);
-      System.out.println(cvGetSize(orgImg));
+      //System.out.println(cvGetSize(orgImg));
       cvCvtColor(orgImg, imgHSV, CV_BGR2HSV);
       // 8-bit 1- color = monochrome
       IplImage imgThreshold = cvCreateImage(cvGetSize(orgImg), 8, 1);
       // cvScalar : ( H , S , V, A)
       cvInRangeS(imgHSV, cvScalar(hueLowerR, 100, 100, 0), cvScalar(hueUpperR, 255, 255, 0), imgThreshold);
+      cvReleaseImage(imgHSV);
+      cvSmooth(imgThreshold, imgThreshold, CV_MEDIAN, 13);
+      // save
+      return imgThreshold;
+  }
+  
+  static IplImage hsvThresholdByPx(IplImage orgImg, int r1,int g1, int b1, int r2, int g2, int b2) {
+      // 8-bit, 3- color =(RGB)
+      IplImage imgHSV = cvCreateImage(cvGetSize(orgImg), 8, 3);
+      //System.out.println(cvGetSize(orgImg));
+      cvCvtColor(orgImg, imgHSV, CV_BGR2HSV);
+      // 8-bit 1- color = monochrome
+      IplImage imgThreshold = cvCreateImage(cvGetSize(orgImg), 8, 1);
+      // cvScalar : ( H , S , V, A)
+      cvInRangeS(imgHSV, cvScalar(r1, g1, b1, 0), cvScalar(r2, g2, b2, 0), imgThreshold);
       cvReleaseImage(imgHSV);
       cvSmooth(imgThreshold, imgThreshold, CV_MEDIAN, 13);
       // save
