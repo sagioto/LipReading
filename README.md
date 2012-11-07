9e124b76ae90685d5c29c2b470a314459fed6e4d
Sagi Bernstein <sagioto@gmail.com>
2012-11-07 22:35:06 +0200

    update to readme and script

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-LipReading [![Build Status](https://secure.travis-ci.org/sagioto/LipsReading.png)](http://travis-ci.org/sagioto/LipsReading)
+LipReading [![Build Status](https://secure.travis-ci.org/sagioto/LipReading.png)](http://travis-ci.org/sagioto/LipReading)
 ==========
 
 ## Goal
@@ -7,7 +7,7 @@ The goal of this project is to extract text words from a video input using visua
 
 ## Contributors
 
-LipsReading is the final project of Ben Gurion University Software Engineering students:
+LipReading is the final project of Ben Gurion University Software Engineering students:
 * [Sagi Bernstein](https://github.com/sagioto) [(LinkedIn)](http://www.linkedin.com/profile/view?id=103685568)
 * [Dor Leitman](https://github.com/dorleitman)
 * [Dagan Sandler](https://github.com/dagansandler) [(LinkedIn)](http://www.linkedin.com/profile/view?id=95457922)
--- opencv-install-ubuntu.sh
+++ opencv-install-ubuntu.sh
@@ -1,31 +1,61 @@
 #!/bin/bash
 
-# Also check there excellent guides: 
 # http://www.ozbotz.org/opencv-installation/
 # http://shambool.com/2012/05/05/install-opencv-24-on-ubuntu-1204/
 
-# 1) Must run ffmpeg-install-ubuntu-12.04.sh before running this script.
+sudo apt-get remove ffmpeg x264 libx264-dev
 
-# 2) Download and install gstreamer.
+sudo apt-get update
+sudo apt-get install build-essential checkinstall git cmake libfaac-dev libjack-jackd2-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libsdl1.2-dev libtheora-dev libva-dev libvdpau-dev libvorbis-dev libx11-dev libxfixes-dev libxvidcore-dev texi2html yasm zlib1g-dev
 
-sudo apt-get install -y libgstreamer0.10-0 libgstreamer0.10-dev gstreamer0.10-tools gstreamer0.10-plugins-base libgstreamer-plugins-base0.10-dev gstreamer0.10-plugins-good gstreamer0.10-plugins-ugly gstreamer0.10-plugins-bad gstreamer0.10-ffmpeg
-
-# 3) Download and install gtk.
-
-sudo apt-get install -y libgtk2.0-0 libgtk2.0-dev
-
-# 4) Download and install libjpeg.
-
-sudo apt-get install -y libjpeg8 libjpeg8-dev
-
-# 5) Download and install install a recent version of v4l (video for linux) from http://www.linuxtv.org/downloads/v4l-utils/. For this guide I used version 0.8.8.
 mkdir ~/src
 cd ~/src
-wget http://www.linuxtv.org/downloads/v4l-utils/v4l-utils-0.8.8.tar.bz2
-tar xvf v4l-utils-0.8.8.tar.bz2
-cd v4l-utils-0.8.8
-make
-sudo make install
+
+if [ "$USER" != "travis" ]; then
+
+	echo "installing x264 ffmpeg gstreamr gtk v4l and libjpeg go for coffee..."
+	# 1.1) Download and install x264
+
+	wget ftp://ftp.videolan.org/pub/videolan/x264/snapshots/last_stable_x264.tar.bz2
+	tar xvf last_stable_x264.tar.bz2
+	cd x264*
+	./configure --enable-static
+	make
+	sudo make install
+
+	# 1.2) Download and install ffmpeg
+
+	cd ~/src
+	wget http://ffmpeg.org/releases/ffmpeg-0.11.1.tar.bz2
+	tar xvf ffmpeg-0.11.1.tar.bz2
+	cd ffmpeg-0.11.1
+
+	/configure --enable-gpl --enable-libfaac --enable-libmp3lame --enable-libopencore-amrnb --enable-libopencore-amrwb --enable-libtheora --enable-libvorbis --enable-libx264 --enable-libxvid --enable-nonfree --enable-postproc --enable-version3 --enable-x11grab --enable-shared
+
+	make
+	sudo make install
+
+	# 2) Download and install gstreamer.
+
+	sudo apt-get install -y libgstreamer0.10-0 libgstreamer0.10-dev gstreamer0.10-tools gstreamer0.10-plugins-base libgstreamer-plugins-base0.10-dev gstreamer0.10-plugins-good gstreamer0.10-plugins-ugly gstreamer0.10-plugins-bad gstreamer0.10-ffmpeg
+
+	# 3) Download and install gtk.
+
+	sudo apt-get install -y libgtk2.0-0 libgtk2.0-dev
+
+	# 4) Download and install libjpeg.
+
+	sudo apt-get install -y libjpeg8 libjpeg8-dev
+
+	# 5) Download and install install a recent version of v4l (video for linux) from http://www.linuxtv.org/downloads/v4l-utils/. For this guide I used version 0.8.8.
+	wget http://www.linuxtv.org/downloads/v4l-utils/v4l-utils-0.8.8.tar.bz2
+	tar xvf v4l-utils-0.8.8.tar.bz2
+	cd v4l-utils-0.8.8
+	make
+	sudo make install
+else
+	echo "travis!! no need to install anything but opencv"
+fi
 
 # 5) Download and install install OpenCV 2.4.2.
 # a) Download OpenCV version 2.4.2 from http://sourceforge.net/projects/opencvlibrary/files/
@@ -60,7 +90,7 @@ sudo make install
 
 export LD_LIBRARY_PATH=/usr/local/lib
 
-# Add the command to your .bashrc file so that you don’t have to enter every time your start a new terminal.
+# Add the command to your .bashrc file so that you donï¿½t have to enter every time your start a new terminal.
 #
 # Alternatively, you can configure the system wide library search path. 
 # Using your favorite editor, add a single line containing the text /usr/local/lib to the end of a file named /etc/ld.so.conf.d/opencv.conf. 
