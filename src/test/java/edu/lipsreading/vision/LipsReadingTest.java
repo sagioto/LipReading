773a6be1a41065c581999d63c7e02a74b0221f27
Sagi Bernstein <sagioto@gmail.com>
2012-11-08 00:09:05 +0200

    changed test

--- src/test/java/edu/lipsreading/vision/LipsReadingTest.java
+++ src/test/java/edu/lipsreading/vision/LipsReadingTest.java
@@ -1,11 +1,15 @@
 package edu.lipsreading.vision;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+
 import org.junit.Test;
 
 public class LipsReadingTest {
 
 	@Test
-	public void dumyTest(){
+	public void dumyTest() throws MalformedURLException, IOException{
+		LipReading.get("https://dl.dropbox.com/u/8720454/Hello%20%281%29.3gp", "hello1.mp4");
 		System.out.println("just pass");
 	}
 }
