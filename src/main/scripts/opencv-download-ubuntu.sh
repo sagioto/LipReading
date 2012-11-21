dbaa03a3799a62fa1dc51e679b3613a4add7d307
Sagi Bernstein <sagioto@gmail.com>
2012-11-22 00:48:00 +0200

    added step in the open cv install

---
+++ src/main/scripts/opencv-download-ubuntu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# http://www.ozbotz.org/opencv-installation/
+# http://shambool.com/2012/05/05/install-opencv-24-on-ubuntu-1204/
+
+# 5) Download and install install OpenCV 2.4.3.
+# a) Download OpenCV version 2.4.3 from http://sourceforge.net/projects/opencvlibrary/files/
+
+cd ~/src
+wget http://downloads.sourceforge.net/project/opencvlibrary/opencv-unix/2.4.3/OpenCV-2.4.3.tar.bz2
+tar xvf OpenCV-2.4.3.tar.bz2
--- src/main/scripts/opencv-install-ubuntu.sh
+++ src/main/scripts/opencv-install-ubuntu.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 
-# http://www.ozbotz.org/opencv-installation/
-# http://shambool.com/2012/05/05/install-opencv-24-on-ubuntu-1204/
-
-# 5) Download and install install OpenCV 2.4.3.
-# a) Download OpenCV version 2.4.3 from http://sourceforge.net/projects/opencvlibrary/files/
-
-cd ~/src
-wget http://downloads.sourceforge.net/project/opencvlibrary/opencv-unix/2.4.3/OpenCV-2.4.3.tar.bz2
-tar xvf OpenCV-2.4.3.tar.bz2
-
-# b) Create a new build directory and run cmake:
-# NOTE: Adding BUILD_PERF_TESTS=0 to disable performance tests because of ffmpeg linking errors.
-
-cd OpenCV-2.4.3/
+cd ~/src/OpenCV-2.4.3/
 mkdir build
 cd build
 cmake -D BUILD_PERF_TESTS=0 -D BUILD_EXAMPLES=0 -D BUILD_TESTS=0 -D CMAKE_BUILD_TYPE=RELEASE ..
