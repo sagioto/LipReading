e24cdfe9298dd5ffd7f37de4e09e8a9076e9b564
Dagan Sandler <dagansandler@gmail.com>
2012-12-09 02:12:12 +0200

    Fixed RotationNormalizer. Added short test.

--- src/main/java/edu/lipreading/normalization/RotationNormalizer.java
+++ src/main/java/edu/lipreading/normalization/RotationNormalizer.java
@@ -7,8 +7,9 @@ import edu.lipreading.Sample;
 /**
  * @author Dagan This class is designed to perform a linear transformation on a
  *         Sample object so the Sample is aligned against X,Y axis. This
- *         Normalizer assumes rotation is smaller than 45 degrees.
- *         It will only transform the sample if the angle is larger than the rotation threshold.
+ *         Normalizer assumes rotation is smaller than 45 degrees. It will only
+ *         transform the sample if the angle is larger than the rotation
+ *         threshold.
  */
 public class RotationNormalizer implements Normalizer {
 
@@ -16,39 +17,54 @@ public class RotationNormalizer implements Normalizer {
 
 	@Override
 	public Sample normalize(Sample sample) {
-		
-		//first calculate the angle (in Radians!).
+
+		// first calculate the angle (in Radians!).
 		double angle = 0;
 
 		int smallestX = 100000000;
+		int largestX = -100000000;
 		int smallestXIndex = 0;
+		int largestXIndex = 0;
 		for (int i = 0; i < sample.getMatrix().size(); i++) {
-			//Looking for the left most point.
+			// Looking for the left most point.
 			List<Integer> vector = sample.getMatrix().get(i);
 			int avg = getAverageX(vector);
 			if (avg < smallestX) {
 				smallestX = avg;
 				smallestXIndex = i;
 			}
+			if (avg > largestX) {
+				largestX = avg;
+				largestXIndex = i;
+			}
 		}
 		List<Integer> leftVector = sample.getMatrix().get(smallestXIndex);
-		//assuming the first point should be aligned to (?,0).
+		List<Integer> rightVector = sample.getMatrix().get(largestXIndex);
+
 		int leftX = leftVector.get(0);
 		int leftY = leftVector.get(1);
-		double z = Math.sqrt(Math.pow(leftX, 2) + Math.pow(leftY, 2));
-		angle = Math.asin(leftY/z);
+		int rightX = rightVector.get(0);
+		int rightY = rightVector.get(1);
 		
+		double dy = rightY - leftY;
+		double dx = rightX - leftX;
+		
+		double centerX = leftX+(dx/2);
+		double centerY = leftY+(dy/2);
+
+		angle = (-1)*Math.atan2(dy, dx);
+
 		if (Math.abs(angle) > ROTATION_THRESHOLD) {
 			for (List<Integer> vector : sample.getMatrix()) {
 				for (int i = 0; i < vector.size(); i += 2) {
-					int x = vector.get(i);
-					int y = vector.get(i + 1);
+					double x = vector.get(i)-centerX;
+					double y = vector.get(i + 1)-centerY;
 					// x' = x*cos(a) - y*sin(a)
 					// y' = x*sin(a) + y*cos(a)
 					vector.set(i,
-							(int) (x * Math.cos(angle) - y * Math.sin(angle)));
+							(int)(Math.round(((x * Math.cos(angle)) - (y * Math.sin(angle))))+centerX));
 					vector.set(i + 1,
-							(int) (x * Math.sin(angle) + y * Math.cos(angle)));
+							(int)(Math.round(((x * Math.sin(angle)) + (y * Math.cos(angle))))+centerY));
 				}
 			}
 		}
