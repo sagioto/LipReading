983598992af0b6d0f1558f737cf5edb0d4483b6f
Sagi Bernstein <sagioto@gmail.com>
2013-01-04 11:16:01 +0200

    trying 6 again

--- src/test/java/edu/lipreading/classification/MultiLayerPerceptronClassifierTest.java
+++ src/test/java/edu/lipreading/classification/MultiLayerPerceptronClassifierTest.java
@@ -54,7 +54,7 @@ public class MultiLayerPerceptronClassifierTest {
 	
 	@BeforeClass
 	public static void loadClassifierModel() throws Exception{
-		Assume.assumeTrue(Utils.isWindows());
+		//Assume.assumeTrue(Utils.isWindows());
 		mpClassifier = new MultiLayerPerceptronClassifier(Constants.MPC_MODEL_URL);
 	}
 	
