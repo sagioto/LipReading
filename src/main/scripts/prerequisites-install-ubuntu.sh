64a97fbada2319f8dbe85a8ad682930e3a59dd89
Sagi Bernstein <sagioto@gmail.com>
2013-01-10 01:31:29 +0200

    split the install ffmpeg into another script

--- src/main/scripts/prerequisites-install-ubuntu.sh
+++ src/main/scripts/prerequisites-install-ubuntu.sh
@@ -30,28 +30,3 @@ tar xvf ffmpeg-1.0.tar.bz2
 cd ffmpeg-1.0
 
 ./configure --enable-gpl --enable-libfaac --enable-libmp3lame --enable-libopencore-amrnb --enable-libopencore-amrwb --enable-libtheora --enable-libvorbis --enable-libx264 --enable-libxvid --enable-nonfree --enable-postproc --enable-version3 --enable-x11grab --enable-shared
-
-make
-sudo make install
-if [ "$USER" != "travis" ]; then
-	# 2) Download and install gstreamer.
-
-	sudo apt-get install -y libgstreamer0.10-0 libgstreamer0.10-dev gstreamer0.10-tools gstreamer0.10-plugins-base libgstreamer-plugins-base0.10-dev gstreamer0.10-plugins-good gstreamer0.10-plugins-ugly gstreamer0.10-plugins-bad gstreamer0.10-ffmpeg
-
-	# 3) Download and install gtk.
-
-	sudo apt-get install -y libgtk2.0-0 libgtk2.0-dev
-
-	# 4) Download and install libjpeg.
-
-	sudo apt-get install -y libjpeg8 libjpeg8-dev
-
-	# 5) Download and install install a recent version of v4l (video for linux) from http://www.linuxtv.org/downloads/v4l-utils/. For this guide I used version 0.8.8.
-	wget http://www.linuxtv.org/downloads/v4l-utils/v4l-utils-0.8.8.tar.bz2
-	tar xvf v4l-utils-0.8.8.tar.bz2
-	cd v4l-utils-0.8.8
-	make
-	sudo make install
-else
-	echo "travis!! no need to install gstreamer, gtk, jpeg, v4l"
-fi
