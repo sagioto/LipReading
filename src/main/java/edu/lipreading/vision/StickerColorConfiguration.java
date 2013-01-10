e588af676903de075c6ded73364f034e4835ed68
Sagi Bernstein <sagioto@gmail.com>
2013-01-10 02:12:33 +0200

    tidy up

--- src/main/java/edu/lipreading/vision/StickerColorConfiguration.java
+++ src/main/java/edu/lipreading/vision/StickerColorConfiguration.java
@@ -6,17 +6,17 @@ import edu.lipreading.Constants;
 
 public class StickerColorConfiguration {
 
-	public static CvScalar UPPER_STICKER_MIN =Constants.UPPER_STICKER_MIN;
-	public static CvScalar UPPER_STICKER_MAX =Constants.UPPER_STICKER_MAX;
+	public static CvScalar UPPER_STICKER_MIN = Constants.UPPER_STICKER_MIN;
+	public static CvScalar UPPER_STICKER_MAX = Constants.UPPER_STICKER_MAX;
                                               
-	public static CvScalar LOWER_STICKER_MIN =Constants.LOWER_STICKER_MIN;
-	public static CvScalar LOWER_STICKER_MAX =Constants.LOWER_STICKER_MAX;
+	public static CvScalar LOWER_STICKER_MIN = Constants.LOWER_STICKER_MIN;
+	public static CvScalar LOWER_STICKER_MAX = Constants.LOWER_STICKER_MAX;
                                               
 	public static CvScalar LEFT_STICKER_MIN = Constants.LEFT_STICKER_MIN;
 	public static CvScalar LEFT_STICKER_MAX = Constants.LEFT_STICKER_MAX;
                                               
-	public static CvScalar RIGHT_STICKER_MIN =Constants.RIGHT_STICKER_MIN;
-	public static CvScalar RIGHT_STICKER_MAX =Constants.RIGHT_STICKER_MAX;
+	public static CvScalar RIGHT_STICKER_MIN = Constants.RIGHT_STICKER_MIN;
+	public static CvScalar RIGHT_STICKER_MAX = Constants.RIGHT_STICKER_MAX;
 	
 
 }
