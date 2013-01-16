47e1b7329f3323b3fead5976e812e580bb57c9db
Sagi Bernstein <sagioto@gmail.com>
2013-01-16 13:15:04 +0200

    reverted the parallel change to the DTW classifier as it produced bad results, doing something like this in the future should be considered

--- src/main/java/edu/lipreading/classification/TimeWarperClassifier.java
+++ src/main/java/edu/lipreading/classification/TimeWarperClassifier.java
@@ -3,52 +3,49 @@ package edu.lipreading.classification;
 import java.util.List;
 
 import edu.lipreading.Constants;
-import edu.lipreading.Parallel;
 import edu.lipreading.Sample;
 import edu.lipreading.Utils;
 
 public class TimeWarperClassifier implements Classifier{
-    private List<Sample> trainingSet;
-    private List<String> vocabulary = Constants.VOCABULARY;
+	private List<Sample> trainingSet;
+	private List<String> vocabulary = Constants.VOCABULARY;
 
 
-    @Override
-    public String test(final Sample test) {
-        final double [] results = new double[vocabulary.size()];
-        final int [] counts = new int[vocabulary.size()];
-        final TimeWarper tw = new TimeWarper();
-        Parallel.For(trainingSet, 
-                new Parallel.Operation<Sample>() {
-            public void perform(Sample training) {
-                if(!test.equals(training)){
-                    for (int i = 0; i < vocabulary.size(); i++) {
-                        if(training.getId().contains(vocabulary.get(i))){
-                            results[i] += tw.dtw(test, training);
-                            counts[i]++;
-                        }
-                    }
-                }
-            };
-        });
-        for (int i = 0; i < vocabulary.size(); i++) {
-            results[i] /= counts[i];
-        }
-        int minIndex = Utils.getMinIndex(results);
-        return vocabulary.get(minIndex);
-    }
+	@Override
+	public String test(final Sample test) {
+		final double [] results = new double[vocabulary.size()];
+		final int [] counts = new int[vocabulary.size()];
+		final TimeWarper tw = new TimeWarper();
+
+		for (Sample training : trainingSet) {
+			if(!test.equals(training)){
+				for (int i = 0; i < vocabulary.size(); i++) {
+					if(training.getId().contains(vocabulary.get(i))){
+						results[i] += tw.dtw(test, training);
+						counts[i]++;
+					}
+				}
+			}
+		}
+		for (int i = 0; i < vocabulary.size(); i++) {
+			results[i] /= counts[i];
+		}
+		int minIndex = Utils.getMinIndex(results);
+		return vocabulary.get(minIndex);
+	}
 
 
-    @Override
-    public void train(List<Sample> trainingSet) {
-        this.trainingSet = trainingSet;
+	@Override
+	public void train(List<Sample> trainingSet) {
+		this.trainingSet = trainingSet;
 
-    }
+	}
 
 
-    @Override
-    public void update(Sample train) {
-        this.trainingSet.add(train);
-    }
+	@Override
+	public void update(Sample train) {
+		this.trainingSet.add(train);
+	}
 
 
 }
