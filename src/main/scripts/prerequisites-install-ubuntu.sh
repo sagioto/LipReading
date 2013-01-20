d041e60a6b97e0ed2ef748c98f42ba5a54da600e
Sagi Bernstein <sagioto@gmail.com>
2013-01-20 03:13:57 +0200

    Update src/main/scripts/prerequisites-install-ubuntu.sh

--- src/main/scripts/prerequisites-install-ubuntu.sh
+++ src/main/scripts/prerequisites-install-ubuntu.sh
@@ -27,7 +27,7 @@ echo "installing ffmepg"
 cd ~/src
 wget http://ffmpeg.org/releases/ffmpeg-1.0.1.tar.bz2
 tar xvf ffmpeg-1.0.1.tar.bz2
-
+cd ffmpeg-1.0.1
 ./configure --enable-gpl --enable-libfaac --enable-libmp3lame --enable-libopencore-amrnb --enable-libopencore-amrwb --enable-libtheora --enable-libvorbis --enable-libx264 --enable-libxvid --enable-nonfree --enable-postproc --enable-version3 --enable-x11grab --enable-shared
 make
 sudo make install
