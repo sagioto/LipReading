ef4be63f457a7249d1571e907258923a7d9c3f4e
Dagan Sandler <dagansandler@gmail.com>
2013-02-24 01:28:26 +0200

    Added small tool to analyze and verify recorded XMLs

---
+++ src/main/java/edu/lipreading/XMLVerifier.java
@@ -0,0 +1,73 @@
+package edu.lipreading;
+
+import java.io.File;
+import java.util.List;
+
+import weka.core.xml.XStream;
+
+public class XMLVerifier {
+
+	/**
+	 * This tool scans a directory that is given as an argument for XML files
+	 * that represent recorded Samples. It builds a Sample from each file and
+	 * scans for <0,0> entries in the vectors, and presents some statistics.
+	 * 
+	 * @param args
+	 *            should contain one argument that is a directory with XML
+	 *            files.
+	 */
+	public static void main(String[] args) {
+		if (args.length < 1) {
+			System.out.println("please supply location of XML files to verify");
+			return;
+		}
+
+		String xmlsLocation = args[0];
+		int numOfInconsistencies = 0;
+		int biggestInconsistency = 0;
+		int numOfFiles = 0;
+		int invalidXMLs = 0;
+		String biggestInconsistentSample = "";
+
+		File xmlPath = new File(xmlsLocation);
+
+		for (File xml : xmlPath.listFiles()) {
+			if (xml.getName().endsWith(".xml")) {
+				try {
+					Sample samp = (Sample) XStream.read(xml);
+					numOfFiles++;
+					int sampInconsistency = 0;
+					for (List<Integer> vector : samp.getMatrix()) {
+						for (int coord : vector) {
+							if (coord == 0) {
+								sampInconsistency++;
+								numOfInconsistencies++;
+							}
+						}
+					}
+					if (biggestInconsistency == 0
+							|| sampInconsistency > biggestInconsistency) {
+						biggestInconsistency = sampInconsistency;
+						biggestInconsistentSample = xml.getName();
+					}
+				} catch (Exception e) {
+					System.out.println("Error parsing XML file: "
+							+ xml.getName());
+					invalidXMLs++;
+				}
+			}
+		}
+
+		numOfInconsistencies = numOfInconsistencies / 2;
+		biggestInconsistency = biggestInconsistency / 2;
+
+		System.out.println("Number of XMLs scanned: " + numOfFiles);
+		System.out.println("Number of Invalid XML Files: " + invalidXMLs);
+		System.out.println("Number of Inconsistencies found: "
+				+ numOfInconsistencies);
+		System.out.println("Sample '" + biggestInconsistentSample
+				+ "' with most inconsistencies contained: "
+				+ biggestInconsistency);
+	}
+
+}
