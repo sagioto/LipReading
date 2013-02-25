0f8164bd179fd7e44fa1e8c04ab4885e59da26a5
Dor Leitman <dorleitman@gmail.com>
2013-02-25 22:47:12 +0200

    added permutationFinder - template only, with no classification yet

---
+++ src/main/java/edu/lipreading/PermutationFinder.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package edu.lipreading;
+
+import java.util.ArrayList;
+
+/**
+ * @author Dor Leitman
+ *
+ */
+public class PermutationFinder {
+	public static int counter;
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		counter = 0;
+		/*
+		for (int i=2; i<=Constants.VOCABULARY.size(); i++){
+			permute(i, 0, new ArrayList<String>());
+		}
+		*/
+		permute(4, 0, new ArrayList<String>()); // TODO Delete
+		System.out.println("Number of permutations: " + counter);
+		
+	}
+	
+	
+	
+	public static void permute(int lengthOfSet, int wordIndexInVocabulary, ArrayList<String> currentSet){
+		if (wordIndexInVocabulary > Constants.VOCABULARY.size()){
+			return;
+		}
+		if (currentSet.size() == lengthOfSet){
+			for (String string : currentSet) {
+				System.out.print(string + " ");
+			}
+			System.out.println();
+			counter++;
+			//TODO Classify & save rate
+		}
+		else{
+			if (wordIndexInVocabulary < Constants.VOCABULARY.size()){
+				@SuppressWarnings("unchecked")
+				ArrayList<String> newSet = (ArrayList<String>) currentSet.clone();
+				newSet.add(Constants.VOCABULARY.get(wordIndexInVocabulary));
+				permute(lengthOfSet, wordIndexInVocabulary + 1, newSet);
+			}
+			permute(lengthOfSet, wordIndexInVocabulary + 1, currentSet);
+			
+		}
+		
+	}
+
+}
